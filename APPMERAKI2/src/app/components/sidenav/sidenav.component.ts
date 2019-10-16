0c05f9dc2b5776df26f22b4410c695c79dd2fc1e
Gusatvo Alarcón <galarcon@meraki-s.com>
2019-10-16 14:34:10 -0500

    correcciones

--- APPMERAKI2/src/app/components/sidenav/sidenav.component.ts
+++ APPMERAKI2/src/app/components/sidenav/sidenav.component.ts
@@ -1,6 +1,6 @@
 import { MediaMatcher } from '@angular/cdk/layout';
-import { ChangeDetectorRef, Component , OnInit, OnDestroy } from '@angular/core';
-import { AuthService} from '../../services/auth.service';
+import { ChangeDetectorRef, Component, OnInit, OnDestroy } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
 import { Observable } from 'rxjs';
 import { User } from '../../services/user.model';
 
@@ -9,41 +9,40 @@ import { User } from '../../services/user.model';
   templateUrl: './sidenav.component.html',
   styleUrls: ['./sidenav.component.css']
 })
-export class SidenavComponent implements OnInit , OnDestroy {
+export class SidenavComponent implements OnInit, OnDestroy {
 
-  private user : User;
+  private user: User;
 
   mobileQuery: MediaQueryList;
 
-  items:string[]=['Bienvenidos','Empezar'];
-  shouldRun =true;
+  items: string[] = ['Bienvenidos', 'Empezar'];
+  shouldRun = true;
   private _mobileQueryListener: () => void;
   router: any;
   afAuth: any;
 
   constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher,
-               public auth:AuthService) {
+    public auth: AuthService) {
     this.mobileQuery = media.matchMedia('(max-width: 600px)');
     this._mobileQueryListener = () => changeDetectorRef.detectChanges();
     this.mobileQuery.addListener(this._mobileQueryListener);
   }
-ngOnInit() {
+
+  ngOnInit() {
     //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
     //Add 'implements OnInit' to the class.
-    this.auth.authState.subscribe((user)=>{
-    this.user = user;
-    console.log(this.user);    
+    this.auth.authState.subscribe((user) => {
+      this.user = user;
+      console.log(this.user);
     })
-    
   }
 
   ngOnDestroy(): void {
     this.mobileQuery.removeListener(this._mobileQueryListener);
   }
 
-  
+
 
 
 
 }
-  
